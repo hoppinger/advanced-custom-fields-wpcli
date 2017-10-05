82b7db99b60b1af574a0677427f7af3c86630982
David Toews <dave@wws-MacBook-Pro.local>
2017-10-05 16:54:58 -0600

    Adjust to use acf export function

--- lib/acfwpcli/field_group.php
+++ lib/acfwpcli/field_group.php
@@ -87,11 +87,11 @@ class FieldGroup {
     // extract field group ID
     acf_extract_var( $field_group, 'ID' );
 
+    //prepare field group
+    $field_group = acf_prepare_field_group_for_export($field_group);
+
     // add to field group
     $field_group['fields'] = $fields;
-
-    //remove _valid setting
-    unset($field_group['_valid']);
 
     return $field_group;
   }
