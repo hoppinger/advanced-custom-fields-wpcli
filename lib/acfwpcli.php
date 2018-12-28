bd4ed95ba0c9797dc3ed020de84765c3bee75293
Andra <andra@hoppinger.com>
2018-12-28 14:14:18 +0100

    Cache loaded, parsed and constructed `ACFWPCLI\FieldGroup`s
    With heavier use of complex field groups, this can shave tens
    of milliseconds off of each request.

--- lib/acfwpcli.php
+++ lib/acfwpcli.php
@@ -30,28 +30,33 @@ class ACFWPCLI {
     $paths    = apply_filters( 'acfwpcli_fieldgroup_paths', $paths );
     $patterns = [];
 
-    foreach ( $paths as $key => $value ) {
-      if ( ! is_dir( $value ) ) {
+    foreach ( $paths as $path ) {
+      if ( ! is_dir( $path ) ) {
         continue;
       }
-      $patterns[ $key ] = trailingslashit( $value ) . '*.json';
+      $patterns[] = trailingslashit( $path ) . '*.json';
     }
 
     $added_groups = [];
-    foreach ( $patterns as $pattern ) {
-      // register the field groups specific for this subsite
-      foreach ( glob( $pattern ) as $file ) {
-        $groups = ACFWPCLI\FieldGroup::from_json_file( $file );
 
-        foreach ( $groups as $group ) {
-          // Don't register group when the group is already in the DB
-          if ( ! in_array( $group['title'] , $db_field_group_titles ) ) {
-            acf_add_local_field_group( $group );
-          }
+    $files = array_reduce($patterns, function($merged, $pattern){ return array_merge($merged, glob( $pattern )); }, []);
 
-          $added_groups[] = $group['title'];
-        }
+    $sha = sha1(implode($files));
+    $transient_key = "sk_acf-groups_$sha";
+    
+    $groups = get_transient($transient_key); # Use cached groups when available
+    if (empty($groups)) {
+      $groups = array_reduce($files, function($merged, $file){ return array_merge($merged, ACFWPCLI\FieldGroup::from_json_file($file)); }, []);
+      set_transient($transient_key, $groups, 24 * HOUR_IN_SECONDS);
+    }
+
+    foreach ($groups as $group) {
+      // Don't register group when the group is already in the DB
+      if (!in_array($group['title'], $db_field_group_titles)) {
+        acf_add_local_field_group($group);
       }
+
+      $added_groups[] = $group['title'];
     }
 
     $this->added_groups = $added_groups;
